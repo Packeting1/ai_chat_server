bd1fce4f61204053b604efbc3a96b94b17043b96
packeting@qq.com <Packeting>
2025-09-25 11:37:17 +0800

    add

--- web_server/app/routing.py
+++ web_server/app/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/stream/?$", consumers.StreamChatConsumer.as_asgi()),
-    # re_path(r"ws/upload/?$", consumers.UploadConsumer.as_asgi()),
+    re_path(r"ws/upload/?$", consumers.UploadConsumer.as_asgi()),
 ]
